919c5807a3672d4ae3fe267deb372630551dc1d7
emifa <emifa@LAPTOP-1TM449QB>
2023-07-09 13:38:52 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-8-LINQ--.NET/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] num = { 1, 5, 4, 8, 9, 10 };
+
+var numEven = from number in num
+              where number % 2 == 0
+              select number;
+
+Console.WriteLine("NUMBER EVEN : ");
+
+foreach(int numb in numEven)
+    Console.WriteLine(numb);
+Console.WriteLine();
+
+var numOdd = num.Where(number => number % 2 != 0);
+
+Console.WriteLine("NUMBER ODD : ");
+
+foreach (int numb in numOdd)
+    Console.WriteLine(numb);
